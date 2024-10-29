bd28c98b784c953af8ce146b23f6444e03711146
speardragon <rkdckdfyyd@naver.com>
2024-10-29 17:59:31 +0900

    refactor: 메인 화면 UX 및 오타 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,22 +7,24 @@ import GITFOLIO_LOGO from "../../public/images/gitfolio-logo.png";
 import Image from "next/image";
 
 export default function Home() {
-  const handleKakaoLogin = () => {
+  const handleGithubLogin = () => {
     window.location.href = `${process.env.NEXT_PUBLIC_API_URL}/oauth2/authorization/github`;
   };
 
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen">
-      {/* <header className="flex w-full justify-end p-2">
-        
-      </header> */}
-      <main className="flex flex-1 justify-center items-center">
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <main className="flex items-center justify-center flex-1">
         <div className="flex flex-col items-center gap-2">
-          <Image alt="github_white_logo" src={GITFOLIO_LOGO} width={300} />
-          <div className="text-blue-600 mb-6">
+          <Image
+            alt="github_white_logo"
+            src={GITFOLIO_LOGO}
+            width={300}
+            priority
+          />
+          <div className="mb-6 text-blue-600">
             한 큐에 만드는 나만의 이력서, <strong>깃트폴리오</strong>
           </div>
-          <Button onClick={handleKakaoLogin} className="flex gap-2">
+          <Button onClick={handleGithubLogin} className="flex gap-2">
             <Image
               alt="github_white_logo"
               src={GITHUB_WHITE_LOGO}
