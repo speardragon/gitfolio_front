be845cca8ec95c6e8ce11ca9ea56c3b8b5435a4f
speardragon <rkdckdfyyd@naver.com>
2024-12-02 20:05:44 +0900

    feat(#80): 이력서 생성 시 템플릿을 선택할 수 있다.

--- src/app/(home)/onboarding/repositories/_components/repository.tsx
+++ src/app/(home)/onboarding/repositories/_components/repository.tsx
@@ -2,30 +2,30 @@
 
 import { Button } from "@/components/ui/button";
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import "moment/locale/ko";
 import { useState } from "react";
 import { useRepositoryQuery } from "../_hooks/useRepositoryQuery";
-import { useAuthStore } from "@/app/store/useAuthStore";
 import MultipleSelector, { Option } from "@/components/ui/multi-selector";
 import { useResumeMutation } from "../_hooks/useResumeMutation";
 import RepositorySkeleton from "./repository-skeleton";
 import Link from "next/link";
 import { Input } from "@/components/ui/input";
 import { Switch } from "@/components/ui/switch";
+import Image from "next/image";
+import RESUME_TEMPLATE from "../../../../../../public/images/resume_template.png";
 import { toast } from "sonner";
+import { TooltipArrow } from "@radix-ui/react-tooltip";
 
 export default function Repository() {
-  const { accessToken } = useAuthStore((state) => state);
-
   const [value, setValue] = useState<Option[]>([]);
   const [requirements, setRequirements] = useState<string>("");
   const [visibility, setVisibility] = useState<boolean>(false);
+  const [selectedTemplate, setSelectedTemplate] = useState<string>("template1");
 
   const { data: repositories } = useRepositoryQuery();
   const { mutate } = useResumeMutation();
@@ -50,8 +50,9 @@ export default function Repository() {
       selectedRepo: value.map((repo) => repo.value),
       requirements: requirements,
       visibility: visibility ? "PUBLIC" : "PRIVATE",
+      template: selectedTemplate,
     };
-    mutate({ accessToken: accessToken!, data });
+    mutate({ data });
     // toast.message("You submitted the following values:", {
     //   description: (
     //     <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
@@ -74,54 +75,100 @@ export default function Repository() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center w-1/2 h-full p-4 my-auto space-y-10">
-      <Card className="w-full ">
-        <CardHeader>
-          <CardTitle className="text-xl">깃허브 레파지토리 선택</CardTitle>
-          <CardDescription className="text-lg">
+    <div className="flex flex-col w-1/2 p-8 space-y-2 overflow-y-auto">
+      <div className="flex flex-col w-full p-4 space-y-4 border-2 rounded-lg">
+        <div className="flex flex-col w-full h-full">
+          <div className="text-xl font-semibold">깃허브 레파지토리 선택</div>
+          <div>
             이력서에 넣고 싶은 프로젝트를 생성하기 위한 레파지토리를
-            선택해주세요(최대 3개)
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="h-full space-y-4">
-          <MultipleSelector
-            className=""
-            hidePlaceholderWhenSelected
-            value={value}
-            onChange={handleValueChange}
-            defaultOptions={options}
-            placeholder="원하는 레포지토리를 최대 3개까지 선택해주세요."
-            emptyIndicator={
-              <p className="text-lg leading-10 text-center text-gray-600 dark:text-gray-400">
-                레포지토리가 존재하지 않습니다.
-              </p>
-            }
-          />
-          <Input
-            type="text"
-            placeholder="더 나은 이력서를 만들기 위한 요청사항을 입력해주세요."
-            value={requirements}
-            onChange={(e) => setRequirements(e.target.value)}
-            className="w-full rounded-md"
-          />
-          <div className="flex flex-row items-center justify-between rounded-lg border p-4">
-            <div className="space-y-0.5">
-              <div className="text-base">공개 여부 설정</div>
-              <div className="text-sm text-gray-400">
-                공개 여부를 체크하시면 커뮤니티에 회원님의 이력서가 공개됩니다!
-              </div>
-            </div>
-            <Switch checked={visibility} onCheckedChange={setVisibility} />
+            선택해주세요.(최대 3개)
           </div>
-        </CardContent>
-      </Card>
+        </div>
+        <MultipleSelector
+          className=""
+          hidePlaceholderWhenSelected
+          value={value}
+          onChange={handleValueChange}
+          defaultOptions={options}
+          placeholder="원하는 레포지토리를 최대 3개까지 선택해주세요."
+          emptyIndicator={
+            <p className="text-lg leading-10 text-center text-gray-600 dark:text-gray-400">
+              레포지토리가 존재하지 않습니다.
+            </p>
+          }
+        />
+        <Input
+          type="text"
+          placeholder="더 나은 이력서를 만들기 위한 요청사항을 입력해주세요."
+          value={requirements}
+          onChange={(e) => setRequirements(e.target.value)}
+          className="w-full py-4 rounded-md"
+        />
+        <div className="flex flex-row items-center justify-between p-4 border rounded-lg">
+          <div className="space-y-0.5">
+            <div className="text-base">공개 여부 설정</div>
+            <div className="text-sm text-gray-400">
+              공개 여부를 체크하시면 커뮤니티에 회원님의 이력서가 공개됩니다!
+            </div>
+          </div>
+          <Switch checked={visibility} onCheckedChange={setVisibility} />
+        </div>
+        <div className="space-y-4 ">
+          <div className="text-base font-medium">이력서 템플릿 선택</div>
+          <div className="grid grid-cols-3 gap-2 justify-items-center">
+            {["template1", "template2", "template3"].map((template, index) => (
+              <label
+                key={template}
+                className="flex flex-col items-center justify-center w-full p-2 space-y-2 border rounded-lg cursor-pointer aspect-square"
+                onClick={() => setSelectedTemplate(template)}
+              >
+                <TooltipProvider>
+                  <Tooltip delayDuration={300}>
+                    <TooltipTrigger className="relative w-full h-full">
+                      {" "}
+                      <Image
+                        src={RESUME_TEMPLATE} // Placeholder 이미지 경로
+                        alt={`Template ${index + 1}`}
+                        fill
+                        className="object-cover rounded-md"
+                      />
+                    </TooltipTrigger>
+                    <TooltipContent
+                      side="left"
+                      className="TooltipContent border w-[400px] h-[600px]"
+                      sideOffset={5}
+                    >
+                      <TooltipArrow />
+                      <Image
+                        src={RESUME_TEMPLATE} // Placeholder 이미지 경로
+                        alt={`Template ${index + 1}`}
+                        fill
+                        className="object-cover border-2 border-gray-500 rounded-md"
+                      />
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+                <input
+                  type="radio"
+                  name="template"
+                  value={template}
+                  checked={selectedTemplate === template}
+                  onChange={() => setSelectedTemplate(template)}
+                  className="mt-2"
+                />
+                <span className="text-sm">{`Template ${index + 1}`}</span>
+              </label>
+            ))}
+          </div>
+        </div>
+      </div>
       <Button
         onClick={onSubmit}
-        className="bg-blue-500 hover:bg-blue-400 w-full"
+        className="w-full bg-blue-500 hover:bg-blue-400"
       >
         이력서 만들러 가기
       </Button>
-      <Link className="text-gray-400 underline" href={"/community"}>
+      <Link className="text-center text-gray-400 underline" href={"/community"}>
         이력서 등록 하지 않고 커뮤니티로 이동
       </Link>
     </div>
