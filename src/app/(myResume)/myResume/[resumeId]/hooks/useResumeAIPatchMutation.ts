5c805f7e3ba82363c65f94cda29574878c67eadf
speardragon <rkdckdfyyd@naver.com>
2024-12-04 20:06:18 +0900

    feat(#76): AI 이력서 수정 기능 추가

---
+++ src/app/(myResume)/myResume/[resumeId]/hooks/useResumeAIPatchMutation.ts
@@ -0,0 +1,43 @@
+import customFetch from "@/app/api/customFetch";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
+
+interface AIPatchBody {
+  selectedText: string;
+  requirement: string;
+}
+
+export function useResumeAIPatchMutation(resumeId: string) {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationKey: ["resumeAIPatch"],
+    mutationFn: async (data: AIPatchBody) => {
+      const response = await customFetch(`/api/resumes/${resumeId}`, {
+        method: "POST",
+        credentials: "include",
+        body: JSON.stringify(data),
+      });
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw {
+          status: response.status,
+          message: errorData.message ?? "에러가 발생했습니다.",
+        };
+      }
+
+      return response.json();
+    },
+    onSuccess: (data) => {
+      toast.success("AI가 성공적으로 이력서를 수정했습니다!", {
+        position: "top-right",
+      });
+      queryClient.invalidateQueries({ queryKey: ["resumes"] });
+    },
+    onError: (error: any) => {
+      toast.error(error.message, {
+        position: "top-right",
+      });
+    },
+  });
+}
